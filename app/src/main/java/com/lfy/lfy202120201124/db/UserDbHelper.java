3a1054ad645eaf1b78c7cedbdcf7ed9d6e98982c
maomu <2048683154@qq.com>
2024-06-26 21:31:00 +0800

    登录的sql以及user实体类创建

---
+++ app/src/main/java/com/lfy/lfy202120201124/db/UserDbHelper.java
@@ -0,0 +1,60 @@
+package com.lfy.lfy202120201124.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class UserDbHelper extends SQLiteOpenHelper {
+    private static UserDbHelper sHelper;
+    private static final String DB_NAME = "user.db";   //数据库名
+    private static final int VERSION = 1;    //版本号
+
+    //必须实现其中一个构方法
+    public UserDbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    //创建单例，供使用调用该类里面的的增删改查的方法
+    public synchronized static UserDbHelper getInstance(Context context) {
+        if (null == sHelper) {
+            sHelper = new UserDbHelper(context, DB_NAME, null, VERSION);
+        }
+        return sHelper;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //创建user_table表
+        db.execSQL("create table user_table(user_id integer primary key autoincrement, " +
+                "username text," +       //用户名
+                "password text," +      //密码
+                "nickname text" +       //
+                ")");
+
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+    //登录
+
+    //注册
+    public int register(String username, String password, String  nickname) {
+        //获取SQLiteDatabase实例
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        //填充占位符
+        values.put("username", username);
+        values.put("password", password);
+        values.put("nickname", nickname);
+        String nullColumnHack = "values(null,?,?,?)";
+        //执行
+        int insert = (int) db.insert("user_table", nullColumnHack, values);
+        db.close();
+        return insert;
+    }
+}
