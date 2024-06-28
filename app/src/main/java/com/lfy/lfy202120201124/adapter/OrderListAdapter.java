266d8a5f9db177ff601dd5aaa077a4d4ac07ee6b
maomu <2048683154@qq.com>
2024-06-28 22:14:16 +0800

    订单页面初始化控件、页面

--- app/src/main/java/com/lfy/lfy202120201124/adapter/OrderListAdapter.java
+++ app/src/main/java/com/lfy/lfy202120201124/adapter/OrderListAdapter.java
@@ -1,31 +1,56 @@
 package com.lfy.lfy202120201124.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.lfy.lfy202120201124.R;
+import com.lfy.lfy202120201124.entity.OrderInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderListAdapter extends RecyclerView.Adapter<OrderListAdapter.MyHolder> {
+    private List<OrderInfo> orderInfoList = new ArrayList<>();
     @NonNull
     @Override
     public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_list_item,null);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-
+        //绑定数据
+        OrderInfo orderInfo = orderInfoList.get(position);
+        //设置数据
+        holder.product_count.setText("x"+orderInfo.getProduct_count());
+        holder.product_img.setImageResource(orderInfo.getProduct_img());
+        holder.product_title.setText(orderInfo.getProduct_title());
+        holder.product_price.setText(orderInfo.getProduct_price()+"");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return orderInfoList.size();
     }
 
     static class MyHolder extends RecyclerView.ViewHolder{
+        ImageView product_img;
+        TextView product_title;
+        TextView product_price;
+        TextView product_count;
         public MyHolder (@NonNull View itemView){
             super(itemView);
+            product_img = itemView.findViewById(R.id.product_img);
+            product_title = itemView.findViewById(R.id.product_title);
+            product_price = itemView.findViewById(R.id.product_price);
+            product_count = itemView.findViewById(R.id.product_count);
         }
 
     }
--- app/src/main/java/com/lfy/lfy202120201124/fragment/OrderFragment.java
+++ app/src/main/java/com/lfy/lfy202120201124/fragment/OrderFragment.java
@@ -2,7 +2,9 @@ package com.lfy.lfy202120201124.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,10 +15,21 @@ import com.lfy.lfy202120201124.R;
 
 public class OrderFragment extends Fragment {
 
+    private View rootView;
+    private RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        rootView = inflater.inflate(R.layout.fragment_order, container, false);
+        //初始化控件
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+        return recyclerView;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
     }
 }
